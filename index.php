b8fc1d610277d006d733f6799048fbd61ac0c81d
Christian Fickinger <mail@christianfickinger.de>
2026-03-23 18:02:38 +0100

    v1.0.0 — Kirby 5 compatible, new settings format
    - Bump version to 1.0.0

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 pwConfig::register('pwmedia', __DIR__ . '/src/config');
 
 Kirby::plugin('kirbydesk/kirbyblock-media', [
-
 	/* -------------- Extensions --------------*/
 	'blueprints' => require_once 'src/extensions/blueprints.php',
 	'snippets' => require_once 'src/extensions/snippets.php',
